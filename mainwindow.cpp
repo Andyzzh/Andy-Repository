5039e6009dc36842af2375fb27196e2dfcc969a4
Andy <zzh18793350309@163.com>
2023-08-20 11:44:24 +0800

    player

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QDebug.h"
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/version.h>
+#include <libavutil/time.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/imgutils.h>
+}
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    qDebug() << avcodec_version();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
